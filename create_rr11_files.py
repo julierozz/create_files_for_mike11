619196fe676e03d18a86c3222ce1cd66f5b16707
julierozenberg <julie.rozenberg@gmail.com>
2014-06-29 18:47:39 -0400

    forgot this file create_rr11_files

---
+++ create_rr11_files.py
@@ -0,0 +1,10 @@
+def create_rr11_files(intro_txtfile,ro_txtfile,rp_cc_txtfile,outputfile):
+    intro_txt=open(intro_txtfile,"r")
+    ro_txt=open(ro_txtfile,"r")
+    rp_cc_txt=open(rp_cc_txtfile,"r")
+    file=open(outputfile,"w")
+    file.write(intro_txt.read()+ro_txt.read()+rp_cc_txt.read())
+    file.close()
+    intro_txt.close()
+    ro_txt.close()
+    rp_cc_txt.close()
